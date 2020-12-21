f7c4146982e27d3042decb6ad4ec25e737c7b59c
OscarCanongo <oscarcanongo@outlook.com>
2020-12-21 12:26:43 -0600

    Validación del formulario

--- upTask/routes/index.js
+++ upTask/routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const proyectosController = require ('../controllers/proyectosController');
     
 router.get('/', proyectosController.proyectosHome);
+router.get('/nuevo-proyecto', proyectosController.formularioProyecto);
+router.post('/nuevo-proyecto', proyectosController.nuevoProyecto);
     
 module.exports = router;
