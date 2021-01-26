1d2926054299b2ded14fc2175d01c276e2d63b4a
OscarCanongo <oscarcanongo@outlook.com>
2021-01-25 20:02:51 -0600

    Conexión a la base de datos

--- devJobs/index.js
+++ devJobs/index.js
@@ -1,7 +1,13 @@
+const mongoose = require('mongoose');
+require('./config/db');
 const express = require('express');
 const router = require('./routes');
 const xphbs = require('express-handlebars');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const mongoStore = require('connect-mongo')(session);
+require('dotenv').config({ path: 'variables.env' });
 
 const app = express();
 
@@ -16,6 +22,16 @@ app.set('view engine', 'handlebars');
 
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new mongoStore({ mongooseConnection: mongoose.connection })
+}));
 
 app.use('/', router);
 
